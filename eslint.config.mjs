8eb90f53aaca727198e3ebce5f6ee81cc471f9cc
ibnabdullah1 <arfathossen541@gmail.com>
2024-09-28 19:55:36 +0600

    Travel Tips & Destination Guides server basic setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      // 'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: ['**/node_modules/', '**/dist/'],
+  }
+);
